8af6b0aa50b4237fb7dc316e869d3ffd0f593077
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-15 14:00:36 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,1 +1,19 @@
+viktors_ingredients = [
+    "чушки", "домати", "моркови", "ябълки",
+    "сол", "черен пипер", "кимион", "зехтин"
+]
 
+georgis_ingredients = (
+    "чушки", "домати", "патладжан", "люти чушки",
+    "олио", "захар", "чубрица", "черен пипер", "врачанска ракия"
+)
+
+shopping_list = viktors_ingredients + list(georgis_ingredients)
+shopping_list.reverse()
+
+unique_ingredients = set(shopping_list)
+
+ingredient_quantities = dict.fromkeys(unique_ingredients, 5)
+ingredient_quantities["skyr"] = 1
+
+number_of_ingredients_to_buy = len(ingredient_quantities)
