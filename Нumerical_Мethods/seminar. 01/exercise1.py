5addd6184d09c3635d55b9031c43cc29ca0d7a03
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-15 14:02:31 +0300

    Update exercise1.py

--- Нumerical_Мethods/seminar. 01/exercise1.py
+++ Нumerical_Мethods/seminar. 01/exercise1.py
@@ -1,1 +1,23 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
+x_values = [0, math.pi / 6, math.pi / 3, math.pi / 2]
+y_values = []
+
+for current in x_values:
+    y_values.append(math.sin(current))
+
+def func(x, x_values, y_values):
+    result = 0
+    for i in range(len(x_values)):
+        term = y_values[i]
+        for j in range(len(x_values)):
+            if i != j:
+                term *= (x - x_values[j]) / (x_values[i] - x_values[j])
+        result += term
+    return result
+
+x = math.pi / 5
+result = func(x, x_values, y_values)
+print(x)
