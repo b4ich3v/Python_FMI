0778599f7c60d64cd667344dae11860f1f8253ef
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-21 14:03:45 +0300

    Update task11.py

--- Exercises/task11.py
+++ Exercises/task11.py
@@ -1,22 +1,22 @@
 def func(n, *args):
-    listOfProcent = [0, 0, 0, 0, 0]
+    list_of_procent = [0, 0, 0, 0, 0]
 
     for value in args:
         if value < 200:
-            listOfProcent[0] += 1
+            list_of_procent[0] += 1
         elif 200 <= value < 400:
-            listOfProcent[1] += 1
+            list_of_procent[1] += 1
         elif 400 <= value < 600:
-            listOfProcent[2] += 1
+            list_of_procent[2] += 1
         elif 600 <= value < 800:
-            listOfProcent[3] += 1
+            list_of_procent[3] += 1
         else:  # value >= 800
-            listOfProcent[4] += 1
+            list_of_procent[4] += 1
 
     if n > 0:
-        listOfProcent = [round((count * 100) / n, 2) for count in listOfProcent]
+        list_of_procent = [round((count * 100) / n, 2) for count in list_of_procent]
 
-    print(listOfProcent)
+    print(list_of_procent)
 
 n = int(input())
 args = []
