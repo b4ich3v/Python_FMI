5942e45fa0bf411aa51067c1500efad99eb70222
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-23 23:45:48 +0300

    Update task.py

--- Challenges/Challenge. 01/task.py
+++ Challenges/Challenge. 01/task.py
@@ -7,16 +7,13 @@ def type_check(arg_type):
             def type_validator(*args, **kwargs):
 
                 def print_error(message_type):
-                    expected_types_str = ''
-                    for i in range(len(expected_types)):
-                        expected_types_str += str(expected_types[i])
-                        if i < len(expected_types) - 1:
-                            expected_types_str += ', '
+                    expected_types_str = ', '.join(map(str, expected_types))
                     print(f"Invalid {message_type} arguments, expected {expected_types_str}!")
 
-                if arg_type == "in":  
+                if arg_type == "in":
                     invalid_args = [current_arg for current_arg in args if type(current_arg) not in expected_types]
-                    if len(invalid_args) > 0:
+                    invalid_kwargs = [current_value for current_value in kwargs.values() if type(current_value) not in expected_types]
+                    if invalid_args or invalid_kwargs:
                         print_error("in")
 
                 result = function(*args, **kwargs)
@@ -31,4 +28,3 @@ def type_check(arg_type):
         return type_wrapper
 
     return type_decorator
-
