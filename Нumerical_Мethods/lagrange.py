d50f89311b4980e6346c678067ab8d6ec1dae2c3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-29 21:41:30 +0200

    Create lagrange.py

---
+++ Нumerical_Мethods/lagrange.py
@@ -0,0 +1,24 @@
+import numpy as np
+import sympy
+import sympy as sp
+
+def lagrange_pol_ve1(nodes, values, x):
+    result = 0
+    for i in range(len(nodes)):
+        term = values[i]
+        for j in range(len(nodes)):
+            if i != j:
+                term *= (x - nodes[j]) / (nodes[i] - nodes[j])
+        result += term
+    return result
+
+def lagrange_pol_ve2(nodes, values):
+    x = sympy.Symbol("x")
+    result = 0
+    for i in range(len(nodes)):
+        term = values[i]
+        for j in range(len(nodes)):
+            if i != j:
+                term *= (x - nodes[j]) / (nodes[i] - nodes[j])
+        result += term
+    return result
