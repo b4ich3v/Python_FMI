8e7638b486241ba05f9a9241af9a153d8f727a1d
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-08 16:30:11 +0200

    Update task.py

--- Challenges/Challenge. 03/task.py
+++ Challenges/Challenge. 03/task.py
@@ -1,8 +1,23 @@
-class ProtectedSection:
+class ExceptionStore:
+    def __init__(self):
+        self.all_exceptions = []
+        self.exception = None
+
+    def log_exception(self, exc_value, exc_type):
+        self.all_exceptions.append((exc_value, exc_type))
+        self.exception = exc_value
+
+
+class ProtectedSection(ExceptionStore):
     def __init__(self, log=(), suppress=()):
+        if not all(isinstance(exc, type) and issubclass(exc, BaseException) for exc in log):
+            raise TypeError("Мust be exception types")
+        if not all(isinstance(exc, type) and issubclass(exc, BaseException) for exc in suppress):
+            raise TypeError("Мust be exception types")
+
+        super().__init__()
         self.log = log
         self.suppress = suppress
-        self.exception = None
 
     def __enter__(self):
         return self
@@ -13,5 +28,9 @@ class ProtectedSection:
                 self.exception = exc_value
                 return True
             elif any(issubclass(exc_type, suppress_exc) for suppress_exc in self.suppress):
+                self.exception = exc_value
                 return True
+            else:
+                self.log_exception(exc_value, exc_type)
+                return False
         return False
