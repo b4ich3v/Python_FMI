fdf059827ce00fa34c24c55a0308e795ed090c9c
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-13 12:51:55 +0200

    Update task.py

--- Challenges/Challenge. 04/task.py
+++ Challenges/Challenge. 04/task.py
@@ -1,51 +1,46 @@
-class LockPicker_OMI0600328:
+class LockPicker_0MI0600328:
     DEFAULT_VALUES = {
-        int: 0,
-        str: "",
-        float: 0.0,
-        bool: False,
-        list: [],
-        dict: {},
+        int: int(),
+        str: str(),
+        float: float(),
+        bool: bool(),
+        list: list(),
+        dict: dict(),
         set: set(),
-        tuple: (),
-        complex: complex(0, 0)
+        tuple: tuple(),
+        complex: complex()
     }
 
     def __init__(self, lock):
         self.lock = lock
-        self.attempted_args = set()
+        self.args = []
 
-    def _handle_type_error(self, exc, args):
+    def _handle_type_error(self, exc):
         if exc.position is None:
-            expected_count = exc.expected
-            return [None] * expected_count
+            return [None] * exc.expected
 
+        args_copy = self.args[:]
         position = exc.position - 1
         expected_type = exc.expected
-        
-        if (position, expected_type) in self.attempted_args:
-            return args
-        
-        args[position] = self.DEFAULT_VALUES.get(expected_type, None)
-        self.attempted_args.add((position, expected_type))
-        return args
 
-    def _handle_value_error(self, ex, args):
-        position = ex.position - 1
-        expected_value = ex.expected
-        args[position] = expected_value
-        return args
+        args_copy[position] = self.DEFAULT_VALUES.get(expected_type, None)
+        return args_copy
+
+    def _handle_value_error(self, exc):
+        args_copy = self.args[:]
+        position = exc.position - 1
+        expected_value = exc.expected
+
+        args_copy[position] = expected_value
+        return args_copy
 
     def unlock(self):
-        args = []
-
         while True:
             try:
-                if self.lock.pick(*args):
+                if self.lock.pick(*self.args):
                     break
-            except Exception as ex:
-                if isinstance(ex, TypeError):
-                    args = self._handle_type_error(ex, args)
-                elif isinstance(ex, ValueError):
-                    args = self._handle_value_error(ex, args)
+            except TypeError as exc:
+                self.args = self._handle_type_error(exc)
+            except ValueError as exc:
+                self.args = self._handle_value_error(exc)
 
