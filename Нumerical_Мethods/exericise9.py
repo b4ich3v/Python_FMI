3a3288eaf7b2fdfbed33490aded87e3502dbe05d
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-12-10 14:46:50 +0200

    Delete Нumerical_Мethods directory

--- Нumerical_Мethods/exericise9.py
+++
@@ -1,5 +0,0 @@
-def divided_difference(nodes, values):
-    if len(nodes) == 1:
-        return values[0]
-    den = nodes[-1] - nodes[0]
-    return (divided_difference(nodes[1:], values[1:]) - divided_difference(nodes[:-1], values[:-1])) / den
