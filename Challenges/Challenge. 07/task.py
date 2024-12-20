60429c1e795c1a6a730e9ba3b57a55fc88e37c91
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-12-20 23:59:10 +0200

    Update task.py

--- Challenges/Challenge. 07/task.py
+++ Challenges/Challenge. 07/task.py
@@ -4,9 +4,9 @@ def lucky():
 
     bits_low = (clue(left_shift=5) >> 5) & 0b111  # bit2, bit1, bit0
     bits_high = clue(right_shift=5) & 0b111  # bit7, bit6, bit5
-    bit4 = 1 if clue(bw_or=239) != 239 else 0 # bit4
-    bit3 = 1 if clue(bw_and=8) else 0 # bit3
+    bit_middle = (1 << 0b1) if clue(bw_or=239) != 239 else 0 # bit4
+    bit_middle = (bit_middle | 0b1) if clue(bw_and=8) else bit_middle # bit4, bit3
 
-    result = (bits_high << 5) | (bit4 << 4) | (bit3 << 3) | bits_low
+    result = (bits_high << 5) | (bit_middle << 3) | bits_low
 
     return result
